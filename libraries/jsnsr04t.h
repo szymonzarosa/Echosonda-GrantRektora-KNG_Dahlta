82fc7f41d66e68b8b5f367186d8714d4554b0dbc
Szymon Zarosa <szymon.zarosa@interia.pl>
2024-10-15 12:18:29 +0200

    Create jsnsr04t.h

---
+++ libraries/jsnsr04t.h
@@ -0,0 +1,34 @@
+/*
+  jsnsr04t.h - Library for using ultrasonic sensor driver JSN-SR-04T.
+  Created by Overflaw, August 28, 2023
+  Released into the public domain.
+*/
+#ifndef JSNSR04T_H
+    #define JSNSR04T_H
+
+    #define JSNSR04T_PULSE_LOW_US           5
+    #define JSNSR04T_PULSE_HIGH_US          30
+    #define METER_CONVERSION_COEFFICIENT    14
+
+    #include <ArduinoLog.h>
+    #include <AsyncDelay.h>
+    #include "Arduino.h"
+
+    class JsnSr04T
+    {
+        public:
+            JsnSr04T(int echoPin, int triggerPin, int logLevel = LOG_LEVEL_VERBOSE);
+            void begin(Stream &serial);
+            int readDistance();
+        private:
+            int _triggerPin;
+            int _echoPin;
+            int _logLevel;
+            Stream* _serial;
+            AsyncDelay _pulseLow;
+            AsyncDelay _pulseHigh;            
+
+            void initiatesPulse();
+            int calculateDistance(long echoDurationUs);
+    };
+#endif
