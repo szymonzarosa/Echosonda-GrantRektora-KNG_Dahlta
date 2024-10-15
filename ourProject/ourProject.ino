8d6e75ec82a8fc1863e48da2d6451f31cb050884
Szymon Zarosa <szymon.zarosa@interia.pl>
2024-10-15 12:03:51 +0200

    Update ourProject.ino

--- ourProject/ourProject.ino
+++ ourProject/ourProject.ino
@@ -3,7 +3,7 @@
 #include "DFRobot_GNSS.h"
 #include <jsnsr04t.h>
 
-// Inicjalizujemy piny dla modułu gnss
+// Inicjalizujemy PINy dla modułu GNSS
 #ifdef  I2C_COMMUNICATION
   DFRobot_GNSS_I2C gnss(&Wire ,GNSS_DEVICE_ADDR);
   #else
@@ -17,60 +17,87 @@
   #endif
 #endif
 
-// Definiujemy piny dla modułu echosondy
+// Definiujemy PINy dla modułu echosondy
 #define ECHO_PIN 3
 #define TRIGGER_PIN 2
+#define LED_R 9
+#define LED_G 8
+#define BUTTON 7
+
 JsnSr04T ultrasonicSensor(ECHO_PIN, TRIGGER_PIN, LOG_LEVEL_VERBOSE);
 
 // Inicjalizujemy zmienną na nazwę pliku
 char filename[50];
 
 void setup() {
+  // Inicjalizujemy LED I przycisk
+  pinMode(LED_R,OUTPUT);
+  pinMode(LED_G,OUTPUT);
+  pinMode(BUTTON,INPUT);
+
+  // Ustawiamy wyjście czerwonego na wysoki a zielonego na niski
+  digitalWrite(LED_R,HIGH);
+  digitalWrite(LED_G,LOW);
+
   // Uruchamiamy port szeregowy, aby wyświetlać komunikaty
   Serial.begin(9600);
   while (!Serial);
 
   // Inicjalizujemy moduł do komunikacji z modułem GNSS
   while(!gnss.begin()){
-    Serial.println("No GNSS Module!");
+    Serial.println("Brak modułu GNSS!");
     delay(1000);
   }
   gnss.enablePower();
   gnss.setGnss(eGPS);
   gnss.setRgbOn();
-  Serial.println("GNSS Module initialised");
+  Serial.println("Inicjalizacja modułu GNSS");
 
-  // Inicjalizyjemy obiekt do komunikacji z echosondą
+  // Inicjalizujemy obiekt do komunikacji z echosondą
   ultrasonicSensor.begin(Serial);
-  Serial.println("Sensor Module initialised");
+  Serial.println("Inicjalizacja modułu czujnika");
 
   // Inicjalizujemy moduł do komunikacji z kartą SD
   while (!SD.begin()) {
-    Serial.println("Card failed, or not present");
+    Serial.println("Błąd wczytania karty lub jej brak");
     delay(1000);
   }
-  Serial.println("SD Card Module initialised");
-
-  // Sprawdzamy czy moduł GNSS połączył się z którąś satelitą, 
+  Serial.println("Zainicjalizowano moduł karty SD");
+  
+  // Sprawdzamy, czy moduł GNSS połączył się z którąś satelitą, 
   // sprawdzając czy lat i lon nie są zerami
   sLonLat_t lat = gnss.getLat();
   sLonLat_t lon = gnss.getLon();
-  int latitude = lat.latitude;
-  int lonitude = lon.lonitude;
+  double latitude = lat.latitude;
+  double longitude = lon.lonitude;
 
-  while (latitude == 0 && lonitude == 0){
-    Serial.println("No Satelits in view!");
-    delay(1000);
+  while (latitude == 0 && longitude == 0){
+    Serial.println("Brak widocznych satelit!");
+    delay(2000);
     lat = gnss.getLat();
     lon = gnss.getLon();
     latitude = lat.latitude;
-    lonitude = lon.lonitude;
+    longitude = lon.lonitude;
   }
 
-  // TODO - potencjalnie można dodać przełącznik do Arduino i uruchamiać nagrywanie danych po przełączeniu
-  // Wystarczy podpiąć go do jednego pina i czekać w pętli while aż będzie miał on stan wysoki
+  // Wystarczy podpiąć go do jednego PINa i czekać  
+  // w pętli while aż będzie miał on stan wysoki
 
-  // Tworzymy nowy plik csv o dacie wczytanej z modułu GNSS
+  // Wszystkie moduły są zainicjalizowane 
+  // Czekanie na kliknięcie przycisku
+  digitalWrite(LED_R,HIGH);
+  digitalWrite(LED_G,HIGH);
+
+  while (!digitalRead(BUTTON)) {
+    Serial.println("Wciśnij przycisk");
+    delay(2000);
+  }
+
+  // Inicjalizacja zczytywania danych - LED świeci się na zielono
+  digitalWrite(LED_R,LOW);
+  digitalWrite(LED_G,HIGH);
+
+  // Tworzymy nowy plik .csv o dacie wczytanej z modułu GNSS
   sTim_t utc = gnss.getUTC();
   int hour = utc.hour;
   int minute = utc.minute;
@@ -79,15 +106,15 @@ void setup() {
   File csvFile = SD.open(filename, FILE_WRITE);
 
   if (!csvFile) {
-    Serial.print("error opening - need to reset Arduino");
+    Serial.print("błąd otwarcia - trzeba zresetować Arduino");
     Serial.println(filename);
     while (1);
   }
   
-  Serial.println("Created file on SD Card!");
+  // Serial.println("Stworzono plik na karcie SD !");
 
   csvFile.println();
-  csvFile.println("year\; month\; day\; hour\; minute\; second\; latDirection\; lonDirection\; latitude\; lonitude\; latDegree\; lonDegree\; distance");
+  csvFile.println("year; month; day; hour; minute; second; latDirection; lonDirection; latitude; longitude; latDegree; lonDegree; depth; high; starID; sog; cog");
   csvFile.close();
 }
 
@@ -97,44 +124,125 @@ void loop() {
   sTim_t date = gnss.getDate();
   sLonLat_t lat = gnss.getLat();
   sLonLat_t lon = gnss.getLon();
-  // double high = gnss.getAlt();
-  // uint8_t starUserd = gnss.getNumSatUsed();
-  // double sog = gnss.getSog();
-  // double cog = gnss.getCog();
+  double high = gnss.getAlt();
+  uint8_t starUserd = gnss.getNumSatUsed();
+  double sog = gnss.getSog();
+  double cog = gnss.getCog();
 
   // Zczytujemy dane z modułu echosondy
   int dist = ultrasonicSensor.readDistance();
 
+  File csvFile = SD.open(filename, FILE_WRITE);
   // Tworzymy wiadomość do zapisania na karcie dla formatu CSV
+
   String dataString = "";
+  // Zapis roku
+  
   dataString += date.year;
-  dataString += "\; ";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Zapis miesiąca
+  
   dataString += date.month;
-  dataString += "\; ";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Zapis dnia 
+  
   dataString += date.date;
-  dataString += "\; ";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Zapis godziny (UTC +00:00)
+  
   dataString += utc.hour;
-  dataString += "\; ";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Zapis minuty
+  
   dataString += utc.minute;
-  dataString += "\ ;";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Zapis sekundy
+  
   dataString += utc.second;
-  dataString += "\ ;";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Kierunek szerokości geograficznej
+  
   dataString += (char)lat.latDirection;
-  dataString += "\ ;";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Kierunek długości geograficznej 
+  
   dataString += (char)lon.lonDirection;
-  dataString += "\ ;";
-  dataString += lat.latitude;
-  dataString += "\ ;";
-  dataString += lon.lonitude;
-  dataString += "\ ;";
-  dataString += lat.latitudeDegree;
-  dataString += "\ ;";
-  dataString += lon.lonitudeDegree;
-  dataString += "\ ;";
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  //Szerokość geograficzna - zapis dziesiętny dla stopni
+  
+  dataString += String(lat.latitude, 6);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Długość geograficzna - zapis dziesiętny dla stopni
+  
+  dataString += String(lon.lonitude, 6);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Szerokość geograficzna - zapis dziesiętny dla minut
+  
+  dataString += String(lat.latitudeDegree, 6);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Długość geograficzna - zapis dziesiętny dla minut
+  
+  dataString += String(lon.lonitudeDegree, 6);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Głębokość pomierzona przez sensor
+  
   dataString += dist;
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Wysokość modułu GNSS
+  
+  dataString += String(high,2);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Liczba widzianych satelitów
+  
+  dataString += starUserd;
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Prędkość poruszania się
+  
+  dataString += String(sog, 2);
+  dataString += "; ";
+  csvFile.print(dataString);
+  dataString = "";
+  // Kąt poruszania się
+  
+  dataString += String(cog, 2);
+  csvFile.println(dataString);
+  dataString = "";
+
+  // Debugowanie danych przed zapisem
+  // Serial.println(dataString);
 
   // Otwieramy plik na karcie SD i zapisujemy dane
-  File csvFile = SD.open(filename, FILE_WRITE);
-  csvFile.println(dataString);
+  // File csvFile = SD.open(filename, FILE_WRITE);
+  // csvFile.println(dataString);
   csvFile.close();
 }
